19be878a5ea7c3bb2404e2f8cfac88a314f0bfe7
Andrey Svyrskyi <akkad1v@gmail.com>
2024-05-20 05:21:42 +0300

    ііі

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-const message = "Hello Andrii";
+import express from 'express';
 
-console.log(message);
+const app = express();
+
+const PORT = 3000;
+
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
